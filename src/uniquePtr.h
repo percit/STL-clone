62403226807657be260dab161bf086b4f83da06c
Marcin Zdunek <marcin.zdunek@siili.com>
2024-04-14 19:28:13 +0200

    Small fixes to move assigment

--- src/uniquePtr.h
+++ src/uniquePtr.h
@@ -1,25 +1,5 @@
 #ifndef UNIQUE_PTR_H
 #define UNIQUE_PTR_H
-/*
-    Ownership: Like unique_ptr, your implementation should enforce exclusive ownership of the managed resource. This means that only one
-    unique_ptr should be able to manage the resource at any given time.
-
-    Resource Management: Your implementation should be responsible for managing the lifetime of the dynamically allocated resource. 
-    This includes properly deallocating the resource when the unique_ptr goes out of scope or is reset.
-
-    Move Semantics: Implement move semantics to allow transferring ownership of the resource from one unique_ptr to another efficiently.
-    This typically involves implementing a move constructor and a move assignment operator.
-
-    Custom Deleter: Optionally, you can support custom deleters to allow users to specify custom cleanup behavior for the managed resource.
-
-    No Copying: Like the standard unique_ptr, your implementation should disable copying to enforce exclusive ownership. This is
-    typically done by deleting the copy constructor and copy assignment operator.
-
-    Pointer-like Interface: Your unique_ptr should provide a pointer-like interface, allowing users to access the managed resource 
-    through member functions like get() and operator->().
-
-    Destructor: Ensure that the destructor properly releases the managed resource, if any, when the unique_ptr goes out of scope.
-*/
 
 
 //copy assigment and copy constructor must be deleted
@@ -47,6 +27,7 @@ public:
   //move constructors
   UniquePtr(UniquePtr&& other): ptr(std::move(other->ptr)) {}
   UniquePtr &operator=(UniquePtr&& other) {
+    if(this == other) return *this;
     ptr = std::move(other->ptr);
     return *this;
   }
